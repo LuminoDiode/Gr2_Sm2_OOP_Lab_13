6a7a606e7c7635a1fc159c4864696483949d0eb8
Rodion Ryzhov <65biso2@gmail.com>
2022-06-12 23:55:03 +0300

    code changes

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gr2_Sm2_OOP_Lab_13
+{
+	class Program
+	{
+		[STAThread]
+		public static void Main()
+		{
+			MainWindow main = new MainWindow();
+			main.ShowDialog();
+		}
+	}
+}
